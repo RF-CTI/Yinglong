f977fee6fc5b4d8e7e01785bcab0809d65f33175
Quanfita <1655037559@qq.com>
2022-09-13 21:45:19 +0800

    update models

--- yinglong_server/models.py
+++ yinglong_server/models.py
@@ -5,8 +5,8 @@ from .extensions import db
 
 class User(db.Model):
     id = db.Column('user_id', db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(64))
     token = db.Column(db.String(64))
     is_login = db.Column(db.Boolean, default=False)
